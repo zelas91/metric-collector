dd7bca484dbf1f95f8209bdd026251d39a62445d
zelas <zelas91@gmail.com>
2023-09-11 17:00:22 +0300

    add migration

--- internal/server/repository/pg_storage.go
+++ internal/server/repository/pg_storage.go
@@ -3,6 +3,10 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/source/file"
+
 	_ "github.com/lib/pq"
 )
 
@@ -14,7 +18,27 @@ type DBStorage struct {
 
 func NewDBStorage(ctx context.Context, dbURL string) *DBStorage {
 	db := newPostgresDB(dbURL)
+	migration(db)
 	return &DBStorage{ctx: ctx, db: db, mem: NewMemStorage()}
+}
+
+func migration(db *sql.DB) {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Errorf("migration error WithInstance, err:%v", err)
+		return
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://./schema",
+		"metrics", driver)
+	if err != nil {
+		log.Errorf("migration NewWithDatabaseInstance, err:%v", err)
+		return
+	}
+	if err := m.Up(); err != nil {
+		log.Errorf("migration UP err : %v", err)
+	}
 }
 
 func newPostgresDB(dbURL string) *sql.DB {
