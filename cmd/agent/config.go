ab7b7fac24533052385a1aa60ef71b9023218b55
zelas <zelas91@gmail.com>
2023-12-07 13:50:27 +0300

    iter 20 add

--- cmd/agent/config.go
+++ cmd/agent/config.go
@@ -15,6 +15,9 @@ var (
 	key            *string
 	rateLimit      *int
 	log            = logger.New()
+	buildVersion   string
+	buildDate      string
+	buildCommit    string
 )
 
 func init() {
@@ -23,6 +26,7 @@ func init() {
 	reportInterval = flag.Int("r", 10, " poll interval ")
 	key = flag.String("k", "", "key hash")
 	rateLimit = flag.Int("l", 1, "rate_limit")
+	printVersion()
 }
 
 type Config struct {
@@ -58,4 +62,15 @@ func NewConfig() *Config {
 	}
 	cfg.BaseURL = fmt.Sprintf("http://%s/updates", cfg.BaseURL)
 	return &cfg
+}
+func printVersion() {
+	fmt.Printf("Build version: %s\n", getBuildValue(buildVersion))
+	fmt.Printf("Build date: %s\n", getBuildValue(buildDate))
+	fmt.Printf("Build commit: %s\n", getBuildValue(buildCommit))
+}
+func getBuildValue(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
 }
