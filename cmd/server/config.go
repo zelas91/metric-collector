9bab36d54b735bcca06f9b6ce15fc33ec2b52577
zelas <zelas91@gmail.com>
2023-12-27 11:33:08 +0300

    Iter22.
    Json config adder

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/caarlos0/env/v6"
 	"github.com/zelas91/metric-collector/internal/logger"
 	"github.com/zelas91/metric-collector/internal/server/config"
+	"os"
+)
+
+const (
+	defaultAddr          = "localhost:8080"
+	defaultCryptoKey     = ""
+	defaultStoreInterval = 1
+	defaultStoreFile     = "/tmp/metrics-db.json"
+	defaultDB            = ""
+	defaultRestore       = true
 )
 
 var (
@@ -20,16 +31,18 @@ var (
 	buildDate     = "N/A"
 	buildCommit   = "N/A"
 	cryptoKey     *string
+	jsonCfg       *string
 )
 
 func init() {
-	addr = flag.String("a", "localhost:8080", "endpoint start server")
-	storeInterval = flag.Int("i", 300, "store interval")
-	restore = flag.Bool("r", true, "load file metrics")
-	filePath = flag.String("f", "/tmp/metrics-db.json", "file path ")
-	database = flag.String("d", "", "Database URL")
+	addr = flag.String("a", defaultAddr, "endpoint start server")
+	storeInterval = flag.Int("i", defaultStoreInterval, "store interval")
+	restore = flag.Bool("r", defaultRestore, "load file metrics")
+	filePath = flag.String("f", defaultStoreFile, "file path ")
+	database = flag.String("d", defaultDB, "Database URL")
 	key = flag.String("k", "", "key hash")
-	cryptoKey = flag.String("crypto-key", "", "private key")
+	cryptoKey = flag.String("crypto-key", defaultCryptoKey, "private key")
+	jsonCfg = flag.String("c", "", "config json")
 	printVersion()
 }
 
@@ -66,7 +79,40 @@ func NewConfig() *config.Config {
 	if cfg.CryptoCertPath == "" {
 		cfg.CryptoCertPath = *cryptoKey
 	}
+	if cfg.JSONConfig != "" {
+		cfg.JSONConfig = *jsonCfg
+	}
+	if cfg.JSONConfig != "" {
+		if data, err := os.ReadFile(cfg.JSONConfig); err == nil {
+			configJSON := &config.Config{}
+			if err = json.Unmarshal(data, configJSON); err != nil {
+				log.Errorf("read json config server err:%v", err)
+				return &cfg
+			}
+			if *cfg.Addr == defaultAddr {
+				cfg.Addr = configJSON.Addr
+			}
+			if cfg.CryptoCertPath == defaultCryptoKey {
+				cfg.CryptoCertPath = configJSON.CryptoCertPath
+			}
+			if *cfg.Restore == defaultRestore {
+				cfg.Restore = configJSON.Restore
+			}
+			if *cfg.FilePath == defaultStoreFile {
+				cfg.FilePath = configJSON.FilePath
+			}
 
+			if *cfg.FilePath == defaultStoreFile {
+				cfg.FilePath = configJSON.FilePath
+			}
+			if *cfg.Database == defaultDB {
+				cfg.Database = configJSON.Database
+			}
+			if *cfg.StoreInterval == defaultStoreInterval {
+				cfg.StoreInterval = configJSON.StoreInterval
+			}
+		}
+	}
 	return &cfg
 }
 
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -1,11 +1,12 @@
 package config
 
 type Config struct {
-	Addr           *string `env:"ADDRESS"`
-	StoreInterval  *int    `env:"STORE_INTERVAL"`
-	FilePath       *string `env:"FILE_STORAGE_PATH"`
-	Restore        *bool   `env:"RESTORE"`
-	Database       *string `env:"DATABASE_DSN"`
+	Addr           *string `env:"ADDRESS" json:"address"`
+	StoreInterval  *int    `env:"STORE_INTERVAL" json:"store_interval"`
+	FilePath       *string `env:"FILE_STORAGE_PATH" json:"store_file"`
+	Restore        *bool   `env:"RESTORE" json:"restore"`
+	Database       *string `env:"DATABASE_DSN" json:"database_dsn"`
 	Key            *string `env:"KEY"`
-	CryptoCertPath string  `env:"CRYPTO_KEY"`
+	CryptoCertPath string  `env:"CRYPTO_KEY" json:"crypto_key"`
+	JSONConfig     string  `env:"CONFIG"`
 }
